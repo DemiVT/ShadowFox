71a2f9707c5e3556e659b61fa36c94fa96913dc3
Viraj Tupe <virajtupe71@gmail.com>
2024-09-08 19:46:32 +0530

    Create variables.py

---
+++ Task 1/variables.py
@@ -0,0 +1,16 @@
+# 1. Create a variable named pi and store the value 22/7 in it
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+# 2. Create a variable called 'for' and assign it a value 4
+# Note: 'for' is a reserved keyword in Python, so using it as a variable name will raise a syntax error
+# Uncommenting the following line will result in a SyntaxError
+# for = 4
+
+# 3. Calculate Simple Interest
+principal = 1000  # Example principal amount
+rate_of_interest = 5  # Example rate of interest
+time = 3  # Time in years
+simple_interest = (principal * rate_of_interest * time) / 100
+print("Simple Interest:", simple_interest)
